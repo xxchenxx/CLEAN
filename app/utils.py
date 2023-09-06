5055a236b4618f489268c605b643689feb35d51c
xxchenxx <hfercxx@hotmail.com>
2023-09-06 01:20:38 +0000

    update

--- app/utils.py
+++ app/utils.py
@@ -136,7 +136,6 @@ def parse_args():
     parser.add_argument('--use_SMILE_cls_token', action="store_true")
     parser.add_argument('--no_wandb', action="store_true")
 
-
     parser.add_argument('--distance_loss_coef', type=float, default=1e-6)
 
 
