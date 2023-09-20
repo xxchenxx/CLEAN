3981f32ed88743c2b16cf8120e63f4be404b007e
xxchenxx <hfercxx@hotmail.com>
2023-09-20 19:47:40 +0000

    update

--- app/esm_source/esm/pretrained.py
+++ app/esm_source/esm/pretrained.py
@@ -85,7 +85,7 @@ def has_emb_layer_norm_before(model_state):
     return any(k.startswith("emb_layer_norm_before") for k, param in model_state.items())
 
 
-def _load_model_and_alphabet_core_v1(model_data, use_adapter, _, _, _, _):
+def _load_model_and_alphabet_core_v1(model_data, use_adapter, _1, _2, _3, _4):
     import esm  # since esm.inverse_folding is imported below, you actually have to re-import esm here
 
     alphabet = esm.Alphabet.from_architecture(model_data["args"].arch)
